2e07de1054ca08e1e100bafc8f0392a46a14639a
Ajmal <ajmalazeez007@gmail.com>
2014-09-20 11:32:24 +0530

    complete proj

---
+++ gen/com/greycodes/health/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.greycodes.health;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/greycodes/health/misc/LoginFragment.java
+++ src/com/greycodes/health/misc/LoginFragment.java
@@ -4,8 +4,6 @@ import android.support.v4.app.Fragment;
 
 public class LoginFragment extends Fragment {
 
-	public LoginFragment() {
-		// TODO Auto-generated constructor stub
-	}
+
 
 }
